b27bbc66811f5b37d9a4e160284e7290c76f0f96
Alex17a <Alexadb1789@gmail.com>
2024-07-22 19:20:50 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
-select count(distinct customer_id) 
+select count(distinct customer_id)
 from customers;
 
 select
-    concat(first_name, ' ', last_name) as seller,
+    concat(e.first_name, ' ', e.last_name) as seller,
     count(*) as operations,
-    floor(sum(price * quantity)) as income
+    floor(sum(p.price * s.quantity)) as income
 from sales as s
 inner join employees as e
     on s.sales_person_id = e.employee_id
@@ -15,8 +15,8 @@ order by income desc
 limit 10;
 
 select
-    concat(first_name, ' ', last_name) as seller,
-    floor(avg(price * quantity)) as average_income
+    concat(e.first_name, ' ', e.last_name) as seller,
+    floor(avg(p.price * s.quantity)) as average_income
 from sales as s
 inner join employees as e
     on s.sales_person_id = e.employee_id
@@ -24,8 +24,8 @@ inner join products as p
     on s.product_id = p.product_id
 group by seller
 having
-    floor(avg(price * quantity)) < (
-        select floor(avg(price * quantity))
+    floor(avg(p.price * s.quantity)) < (
+        select floor(avg(p.price * s.quantity))
         from sales as s
         inner join products as p
             on s.product_id = p.product_id
@@ -33,16 +33,16 @@ having
 order by average_income;
 
 select
-    concat(first_name, ' ', last_name) as seller,
-    to_char(sale_date, 'day') as day_of_week,
-    floor(sum(price * quantity)) as income
+    concat(e.first_name, ' ', e.last_name) as seller,
+    to_char(s.sale_date, 'day') as day_of_week,
+    floor(sum(p.price * s.quantity)) as income
 from sales as s
 inner join employees as e
     on s.sales_person_id = e.employee_id
 inner join products as p
     on s.product_id = p.product_id
-group by seller, day_of_week, to_char(sale_date, 'id')
-order by to_char(sale_date, 'id'), seller;
+group by seller, day_of_week, to_char(s.sale_date, 'id')
+order by to_char(s.sale_date, 'id'), seller;
 
 select
     case
@@ -50,27 +50,26 @@ select
         when age between 26 and 40 then '26-40'
         when age > 40 then '40+'
     end as age_category,
-    COUNT(age) as age_count
+    count(age) as age_count
 from customers
-group by 1
-order by 1;
+group by age_category
+order by age_category;
 
 select
-    to_char(sale_date, 'YYYY-MM') as selling_month,
+    to_char(s.sale_date, 'YYYY-MM') as selling_month,
     count(distinct c.customer_id) as total_customers,
-    floor(sum(price * quantity)) as income
+    floor(sum(p.price * s.quantity)) as income
 from sales as s
 inner join customers as c
     on s.customer_id = c.customer_id
 inner join products as p
     on s.product_id = p.product_id
-group by 1
-order by 1;
+group by selling_month
+order by selling_month;
 
-select distinct on (customer_id)
+select distinct on (s.customer_id)
     concat(c.first_name, ' ', c.last_name) as customer,
-    s.sale_date,
-    concat(e.first_name, ' ', e.last_name)
+    s.sale_date
 from sales as s
 inner join customers as c
     on s.customer_id = c.customer_id
@@ -78,5 +77,5 @@ inner join products as p
     on s.product_id = p.product_id
 inner join employees as e
     on s.sales_person_id = e.employee_id
-where price = 0
+where p.price = 0
 order by c.customer_id, s.sale_date;
